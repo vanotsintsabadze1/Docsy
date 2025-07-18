bed395f30263f05b3d1dc67b8c33acb8ce777e3b
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-07-19 03:28:37 +0400

    Add persistence layer with PostgreSQL support
    - Updated `Docsy.API.csproj` to include new package and project references.
    - Added `ConnectionStrings` section in `appsettings.json` for PostgreSQL.
    - Created `AppDbContext` for database context management.
    - Implemented `ServiceCollectionExtensions` for service registration.
    - Added `ConnectionStrings` class for strong-typed connection settings.

---
+++ Docsy.Persistence/Options/ConnectionStrings.cs
@@ -0,0 +1,6 @@
+﻿namespace Docsy.Persistence.Options;
+
+public class ConnectionStrings
+{
+    public required string DefaultConnection { get; set; }
+}
---
+++ Docsy.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Docsy.Persistence.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Docsy.Persistence;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>();
+        services.Configure<ConnectionStrings>(configuration.GetSection(nameof(ConnectionStrings)));
+        return services;
+    }
+}
